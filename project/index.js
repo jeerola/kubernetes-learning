fea0a1d22999a28b9bef9288ce2b8b41aafd7fec
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-06-17 13:31:57 +0300

    Exercise 1.2 completed

---
+++ project/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const todoApp = express();
+const port = process.env.PORT || 3000;
+
+todoApp.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
