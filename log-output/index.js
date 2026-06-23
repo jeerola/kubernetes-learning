64c3d7c967e5142b6cec093e6bef7c2e34c082af
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-06-23 12:06:02 +0300

    Exercise 1.7 completed

--- log-output/index.js
+++ log-output/index.js
@@ -1,7 +1,20 @@
 import { randomUUID } from "crypto";
+import express from "express";
+
+const logOutput = express();
+const port = process.env.PORT || 3000;
+
+logOutput.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
 
 const randomHash = randomUUID();
+const getTimeStamp = () => new Date().toISOString();
 
 setInterval(() => {
-  console.log(`${new Date().toISOString()}: ${randomHash}`);
+  console.log(`${getTimeStamp()}: ${randomHash}`);
 }, 5000);
+
+logOutput.get("/", (req, res) => {
+  res.send(`${getTimeStamp()}: ${randomHash}`);
+});
