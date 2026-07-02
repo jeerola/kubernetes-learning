1a8140e522577d9dbb5d47e3409bcfdd5e7dc8a0
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-07-02 19:18:23 +0300

    exercise 2.5 complete

--- log-output/log-output-reader/index.js
+++ log-output/log-output-reader/index.js
@@ -10,16 +10,25 @@ logOutput.listen(port, () => {
 
 logOutput.get("/", async (req, res) => {
   try {
-
     const hashContent = fs.readFileSync(
       "/usr/src/app/files/hash-file.txt",
       "utf-8",
     );
 
-    const pingContent = await fetch("http://pingpong:3456/pings")
+    const configContent = fs.readFileSync(
+      "/usr/src/app/config/information.txt",
+      "utf-8",
+    );
+
+    const pingContent = await fetch("http://pingpong:3456/pings");
     const pings = await pingContent.text();
 
-    res.status(200).send(`${hashContent} \n Ping / Pongs: ${pings}`);
+    res.type("text/plain");
+    res
+      .status(200)
+      .send(
+        `file content: ${configContent}env variable: MESSAGE=${process.env.MESSAGE}\n${hashContent}\nPing / Pongs: ${pings}`,
+      );
   } catch (err) {
     console.error(err);
     res.status(500).send("File not available");
